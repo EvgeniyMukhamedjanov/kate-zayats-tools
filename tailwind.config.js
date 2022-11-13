f47588257569e3b5f46c8566836206a17ba9c5ae
Evgeniy <evgeniymuhamedjanov@gmail.com>
2022-11-13 20:40:11 +0500

    Complete Idea Matrix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,17 @@
 module.exports = {
   content: ["./src/**/*.js", "./docs/**/*.html"],
   theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'color': {
+        'general': '#F64C0E',
+        1: '#0D0D0D',
+        2: '#D1DAE5',
+        3: '#81898D',
+      },
+      // ...
+    },
     extend: {},
   },
   plugins: [],
